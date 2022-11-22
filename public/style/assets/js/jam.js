ca14861da42f73cd1f8c04491db4b7d24f661cbf
Hafidz <hafidzfadhillah606@gmail.com>
2022-11-22 19:38:01 +0700

    Template For BackEnd Ver-1

---
+++ public/style/assets/js/jam.js
@@ -0,0 +1,17 @@
+function showTime(){
+	var tw = new Date();
+    if (tw.getTimezoneOffset() == 0) (a=tw.getTime() + ( 7 *60*60*1000))
+    else (a=tw.getTime());
+    tw.setTime(a);
+
+    var tahun= tw.getFullYear ();
+    var hari= tw.getDay ();
+    var bulan= tw.getMonth ();
+    var tanggal= tw.getDate ();
+    var hariarray=new Array("Minggu,","Senin,","Selasa,","Rabu,","Kamis,","Jum'at,","Sabtu,");
+    var bulanarray=new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
+
+    document.getElementById("DisplayClock").innerHTML =" "+tanggal+" "+bulanarray[bulan]+" "+tahun+" | Jam " + ((tw.getHours() < 10) ? "0" : "") + tw.getHours() + ":" + ((tw.getMinutes() < 10)? "0" : "") + tw.getMinutes() + (" WIB ");
+}
+
+showTime();
