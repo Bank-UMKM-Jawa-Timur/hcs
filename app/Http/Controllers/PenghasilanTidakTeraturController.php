d38ca3ed6ca0ca3c1c30eabe2622bc034d596903
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-01-04 21:10:53 +0700

    fix: gajipajak view

--- app/Http/Controllers/PenghasilanTidakTeraturController.php
+++ app/Http/Controllers/PenghasilanTidakTeraturController.php
@@ -87,7 +87,8 @@ class PenghasilanTidakTeraturController extends Controller
             'jamsostek' => $totalGaji,
             'penghasilan' => $ptt,
             'bonus' => $bonus,
-            'tahun' => $request->tahun
+            'tahun' => $request->tahun,
+            'request' => $request,
         ]);
     }
 
