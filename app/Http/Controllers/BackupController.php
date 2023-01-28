14ffc5139a2843aa21f4b747622cb15edfe06535
LuciferLovesMe <abhiemsmile@gmail.com>
2023-01-28 20:28:11 +0700

    db backup

---
+++ app/Http/Controllers/BackupController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class BackupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('backup.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Artisan::call('backup:run');
+        $path = storage_path('app/laravel-backup/*');
+        $latest_ctime = 0;
+        $latest_filename = '';
+        $files = glob($path);
+        foreach($files as $file)
+        {
+                if (is_file($file) && filectime($file) > $latest_ctime)
+                {
+                        $latest_ctime = filectime($file);
+                        $latest_filename = $file;
+                }
+        }
+        return response()->download($latest_filename);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- config/filesystems.php
+++ config/filesystems.php
@@ -36,6 +36,11 @@ return [
             'throw' => false,
         ],
 
+        'backup' => [
+            'driver' => 'local',
+            'root' => storage_path('app/backups')
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
