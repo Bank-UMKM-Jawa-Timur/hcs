d33d03abf62999bd8c42aca5d36f08063d4cc6f3
Hafidz Fadhillah Febrianto <63035660+Hafidz1121@users.noreply.github.com>
2023-01-31 13:16:26 +0700

    Add Pendidikan (Klasifikasi Data)

---
+++ app/Enum/PendidikanKaryawan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enum;
+
+enum PendidikanKaryawan:string {
+    case SD = 'SD';
+    case SMP = 'SMP';
+    case SLTA = 'SLTA';
+    case SMK = 'SMK';
+    case S1 = 'S1';
+    case S2 = 'S2';
+    case S3 = 'S3';
+}
