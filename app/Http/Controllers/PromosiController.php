3c7b17aa765069b2cbcbd0ac35e323a857b396fb
LuciferLovesMe <abhiemsmile@gmail.com>
2023-02-02 09:32:12 +0700

    fixing index

--- app/Http/Controllers/PromosiController.php
+++ app/Http/Controllers/PromosiController.php
@@ -94,7 +94,7 @@ class PromosiController extends Controller
             if($typeLama == 1) {
                 $promosiLama->kantor_lama = isset($entityLama->subDiv) ?
                 $entityLama->subDiv->nama_subdivisi . " (Pusat)":
-                $entityLama->div->nama_divisi." (Pusat)";
+                "Pusat";
             }
 
             return $promosiLama;
