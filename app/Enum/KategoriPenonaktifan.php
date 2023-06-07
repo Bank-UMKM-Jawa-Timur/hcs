f8c3c6481ee4c2a23c06d9702839b906d94aebc4
LuciferLovesMe <abhiemsmile@gmail.com>
2023-06-07 20:36:49 +0700

    add kategori penonaktifan

--- app/Enum/KategoriPenonaktifan.php
+++ app/Enum/KategoriPenonaktifan.php
@@ -4,8 +4,10 @@ namespace App\Enum;
 
 enum KategoriPenonaktifan: string
 {
-    case RESIGN = 'Resign';
+    case RESIGN = 'Mengundurkan Diri';
     case PENSIUN = 'Pensiun';
     case MENINGGAL = 'Meninggal Dunia';
     case PHK = 'Pemutusan Hubungan Kerja (PHK)';
+    case TIDAKDIPERPANJANG = 'Tidak Diperpanjang';
+    case KONTRAKPERPANJANGAN = 'Kontrak Perpanjangan';
 }
