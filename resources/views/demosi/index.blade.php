17ae4867667ab31f33987b666ca357bca43e687d
Okan Sajaaa <okannolgenol@gmail.com>
2023-11-25 01:54:27 +0700

    feat/demosi:fix number paginate

--- resources/views/demosi/index.blade.php
+++ resources/views/demosi/index.blade.php
@@ -73,6 +73,10 @@
                                 <tbody>
                                     @php
                                         $i = 1;
+                                        $page = isset($_GET['page']) ? $_GET['page'] : 1;
+                                        $page_length = isset($_GET['page_length']) ? $_GET['page_length'] : 10;
+                                        $number = $page == 1 ? 1 : $page * $page_length - $page_length + 1;
+                                        $i = $page == 1 ? 1 : $number;
                                     @endphp
                                     @foreach ($data as $item)
                                         <tr>
