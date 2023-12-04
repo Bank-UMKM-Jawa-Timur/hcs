39defeadddd48aea3ab973ca762ca7ac8efe34df
29rifjanj <rjndla@gmail.com>
2023-12-04 17:25:07 +0700

    Update index.blade.php

--- resources/views/bonus/index.blade.php
+++ resources/views/bonus/index.blade.php
@@ -62,7 +62,7 @@
                                         <td>{{ $item->total_data }}</td>
                                         <td>Rp {{ number_format($item->jumlah_nominal, 0,',','.') }}</td>
                                         <td>{{ \Carbon\Carbon::parse($item->new_date)->translatedFormat('d F Y') }}</td>
-                                        <td><a href="{{ route('bonus.detail',[$item->id_tunjangan,$item->new_date]) }}">Detail</a></td>
+                                        <td><a href="{{ route('bonus.detail',[$item->id_tunjangan,$item->new_date]) }}" class="btn btn-primary">Detail</a></td>
                                     </tr>
                                 @endforeach
                             </tbody>
