4ada11bf6364167ca3b3ea6a047b8cd5235c14f1
oong <37205723+oong26@users.noreply.github.com>
2023-12-08 11:34:12 +0700

    develop-payroll : fix variable dataSPD on import pengahsilan tidak teratur

--- resources/views/penghasilan/add.blade.php
+++ resources/views/penghasilan/add.blade.php
@@ -35,7 +35,7 @@
                     <label for="id_tunjangan">Penghasilan:</label>
                     <select name="id_tunjangan" id="" class="form-control" required>
                         <option value="">--- Pilih Penghasilan ---</option>
-                        @foreach ($dataSPD as $item)
+                        @foreach ($data as $item)
                             <option value="{{ $item->id }}">{{ $item->nama_tunjangan }}</option>
                         @endforeach
                     </select>
