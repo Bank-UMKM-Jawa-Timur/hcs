c75ba05c07a728911524c7b8c4446f0a3197ef3d
Okan Sajaaa <okannolgenol@gmail.com>
2023-12-10 15:57:20 +0700

    Revert "fix: role detail"  This reverts commit 61efbcfe8087e22e2a8cb564d139308cffa667b5.

---
+++ app/Repository/RolesRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repository;
+
+use App\Http\Controllers\Utils\PaginationController;
+use App\Models\CabangModel;
+use App\Models\KaryawanModel;
+use App\Service\EntityService;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+
+class RolesRepository
+{
+    private $param;
+
+    public function getRoles($search, $limit=10, $page=1) {
+        $role_permissions = Role::when($search, function ($query) use ($search) {
+            $query->where('name', 'like', "%$search%");
+
+        })
+        ->paginate($limit);
+        return $role_permissions;
+    }
+
+    public function getPermission() {
+        return DB::table('permissions')->get();
+    }
+
+    public function getRoleId($id) {
+        $this->param['data'] = Role::find($id);
+        $this->param['dataPermissions'] = DB::table('permissions')
+            ->get();
+        $selected = DB::table('role_has_permissions')
+            ->where('role_id', $id)
+            ->get();
+        $arraySelected = array();
+        foreach($selected as $item){
+            array_push($arraySelected, $item->permission_id);
+        }
+        $this->param['dataPermissionsSelected'] = $arraySelected;
+        return $this->param;
+    }
+}
