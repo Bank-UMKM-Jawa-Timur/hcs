ae14a0f75beb83a42e99cd2bc643e56165e87349
oong <37205723+oong26@users.noreply.github.com>
2023-12-12 14:13:33 +0700

    feat/tunjangan : remove validasi uang transport & makan on edit

--- resources/views/penghasilan-teratur/edit.blade.php
+++ resources/views/penghasilan-teratur/edit.blade.php
@@ -135,40 +135,12 @@
 
                 // 11 = tranport, 12 = pulsa, 13 = vitamin, 14 = uang makan
 
-                if (value == 11 || value == 14) {
-                    if (tanggal == 25) {
-                        var message = "success"
-                        $('#error-penghasilan').addClass('d-none')
-                    } else {
-                        if (value == 12) {
-                            var message = 'Transaksi pulsa hanya bisa dilakukan pada tanggal 1 sampai 10'
-                            nmbr++
-                        } else if (value == 13) {
-                            var message = 'Transaksi vitamin hanya bisa dilakukan pada tanggal 1 sampai 5'
-                            nmbr++
-                        } else if (value == 11) {
-                            var message = 'Transaksi transport hanya bisa dilakukan pada tanggal 25'
-                            nmbr++
-                        } else if (value == 14) {
-                            var message = 'Transaksi uang makan hanya bisa dilakukan pada tanggal 25'
-                            nmbr++
-                        }
-                        // alertWarning(message)
-                        $('#error-penghasilan').removeClass('d-none').html(message)
-                        $("#penghasilan").val("")
-                    }
-                } else if (value == 12) {
+                if (value == 12) {
                     if (tanggal >= 1 && tanggal <= 10){
                         var message = "success"
                         $('#error-penghasilan').addClass('d-none')
                     } else {
-                        if (value == 11) {
-                            var message = 'Transaksi transport hanya bisa dilakukan pada tanggal 25'
-                            nmbr++
-                        } else if (value == 14) {
-                            var message = 'Transaksi uang makan hanya bisa dilakukan pada tanggal 25'
-                            nmbr++
-                        } else if (value == 12) {
+                        if (value == 12) {
                             var message = 'Transaksi pulsa hanya bisa dilakukan pada tanggal 1 sampai 10'
                         } else if (value == 13 && tanggal > 5) {
                             var message = 'Transaksi pulsa hanya bisa dilakukan pada tanggal 1 sampai 10'
@@ -184,23 +156,19 @@
                         $('#error-penghasilan').addClass('d-none')
                     }
                     else {
-                        if (value == 11) {
-                            var message = 'Transaksi transport hanya bisa dilakukan pada tanggal 25'
-                            nmbr++
-                        } else if (value == 12) {
+                        if (value == 12) {
                             var message = 'Transaksi pulsa hanya bisa dilakukan pada tanggal 1 sampai 10'
                             nmbr++
                         } else if (value == 13) {
                             var message = 'Transaksi vitamin hanya bisa dilakukan pada tanggal 1 sampai 5'
-                            nmbr++
-                        } else if (value == 14) {
-                            var message = 'Transaksi uang makan hanya bisa dilakukan pada tanggal 25'
                             nmbr++
                         }
                         // alertWarning(message)
                         $('#error-penghasilan').removeClass('d-none').html(message)
                         $("#penghasilan").val("")
                     }
+                } else {
+                    $('#error-penghasilan').addClass('d-none')
                 }
             })
 
