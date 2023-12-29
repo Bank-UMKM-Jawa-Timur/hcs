adc8c80b6cff389340454ae0764aed837fdc3d7a
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2023-12-29 22:19:45 +0700

    feat/master-user: list pejabar sementara change to pagination

--- app/Http/Controllers/PejabatSementaraController.php
+++ app/Http/Controllers/PejabatSementaraController.php
@@ -22,12 +22,18 @@ class PejabatSementaraController extends Controller
         $this->repo = new PejabatSementaraRepository;
     }
 
-    public function index()
+    public function index(Request $request)
     {
         if (!auth()->user()->can('manajemen karyawan - data penjabat sementara')) {
             return view('roles.forbidden');
         }
-        $pjs = PjsModel::with(['karyawan', 'jabatan'])->get();
+        $limit = $request->has('page_length') ? $request->get('page_length') : 10;
+        $page = $request->has('page') ? $request->get('page') : 1;
+
+        $karyawanRepo = new PejabatSementaraRepository();
+        $search = $request->get('q');
+        $pjs = $karyawanRepo->getAllPejabatSementara($search, $limit, $page);
+
         return view('pejabat-sementara.index', compact('pjs'));
     }
 
