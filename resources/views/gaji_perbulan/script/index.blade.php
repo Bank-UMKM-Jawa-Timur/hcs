a7275c5d156e90523d6ba3552ebb6275c73d8662
oong <37205723+oong26@users.noreply.github.com>
2024-01-08 02:28:32 +0700

    fix-penghasilan : change notif empty data final proses on proses penghasilan

--- resources/views/gaji_perbulan/script/index.blade.php
+++ resources/views/gaji_perbulan/script/index.blade.php
@@ -1,6 +1,19 @@
 @section('custom_script')
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script>
+        $('#uploadFile').on('click',function() {
+            let batch_id = $(this).data('batch_id');
+            let target = $(this).data('target');
+
+            $(`${target} #id`).val(batch_id);
+        })
+        document.querySelector('.custom-file-input').addEventListener('change', function (e) {
+            var name = document.getElementById("upload_csv").files[0].name;
+            var nextSibling = e.target.nextElementSibling
+            nextSibling.innerText = name
+        });
+
         $(".btn-proses").on('click', function(e){
             loadDataPenghasilan()
             $('#proses-modal').modal('show'); 
