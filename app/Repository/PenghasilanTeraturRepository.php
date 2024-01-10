77ef9f31d510e618d2aef91edb5c7eea8c43b256
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2024-01-10 12:22:23 +0700

    fix-import-data: fix download vitamin

--- app/Repository/PenghasilanTeraturRepository.php
+++ app/Repository/PenghasilanTeraturRepository.php
@@ -102,17 +102,33 @@ class PenghasilanTeraturRepository
 
     public function excelVitamin($bulan, $tahun){
         $cabang = CabangModel::select('kd_cabang')->pluck('kd_cabang');
-        $data = DB::table('transaksi_tunjangan')->select(
-            'transaksi_tunjangan.nip',
-            'k.nama_karyawan',
-            'k.no_rekening',
-            'transaksi_tunjangan.nominal as vitamin'
-        )
-            ->join('mst_karyawan as k', 'k.nip', 'transaksi_tunjangan.nip')
-            ->where('id_tunjangan', 13) //vitamin
-            ->whereMonth('transaksi_tunjangan.tanggal', $bulan)
-            ->whereYear('transaksi_tunjangan.tanggal', $tahun)
-            ->get();
+        if (auth()->user()->kd_cabang != null) {
+            $data = DB::table('transaksi_tunjangan')->select(
+                'transaksi_tunjangan.nip',
+                'k.nama_karyawan',
+                'k.no_rekening',
+                'transaksi_tunjangan.nominal as vitamin'
+            )
+                ->join('mst_karyawan as k', 'k.nip', 'transaksi_tunjangan.nip')
+                ->where('id_tunjangan', 13) //vitamin
+                ->where('k.kd_entitas', auth()->user()->kd_cabang)
+                ->whereMonth('transaksi_tunjangan.tanggal', $bulan)
+                ->whereYear('transaksi_tunjangan.tanggal', $tahun)
+                ->get();
+        }else{
+            $data = DB::table('transaksi_tunjangan')->select(
+                'transaksi_tunjangan.nip',
+                'k.nama_karyawan',
+                'k.no_rekening',
+                'transaksi_tunjangan.nominal as vitamin'
+            )
+                ->join('mst_karyawan as k', 'k.nip', 'transaksi_tunjangan.nip')
+                ->where('id_tunjangan', 13) //vitamin
+                ->whereMonth('transaksi_tunjangan.tanggal', $bulan)
+                ->whereYear('transaksi_tunjangan.tanggal', $tahun)
+                ->get();
+
+        }
 
         return $data;
     }
