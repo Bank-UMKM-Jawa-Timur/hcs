db4dd99b673c37c462d347b70f7d4b3a7a03e807
LuciferLovesMe <abhiemsmile@gmail.com>
2024-01-11 18:49:52 +0700

    feat-laporan-rekap: create feature rekap tetap

---
+++ resources/views/rekap-tetap/exports/export.blade.php
@@ -0,0 +1,112 @@
+<table class="table whitespace-nowrap table-bordered" id="table" style="width: 100%;">
+    <thead class="text-primary" style="border:1px solid #e3e3e3 !important">
+        <tr>
+            <th rowspan="2" class="text-center">NO</th>
+            <th rowspan="2" class="text-center">NAMA KARYAWAN</th>
+            <th rowspan="2" class="text-center">ALAMAT</th>
+            <th rowspan="2" class="text-center">L/P</th>
+            <th rowspan="2" class="text-center">JABATAN</th>
+            <th rowspan="2" class="text-center">K/TK</th>
+            <th rowspan="2" class="text-center">MASA KERJA</th>
+            <th colspan="3" class="text-center">PENGHASILAN BRUTO</th>
+            <th colspan="2" class="text-center">BIAYA JABATAN</th>
+            <th rowspan="2" class="text-center">PENGHASILAN NETO</th>
+            <th rowspan="2" class="text-center">PENGHASILAN NETO SETAHUN/DISETAHUNKAN</th>
+            <th rowspan="2" class="text-center">PTKP</th>
+            <th rowspan="2" class="text-center">PKP</th>
+            <th rowspan="2" class="text-center">PPH 21 TERUTANG</th>
+            <th rowspan="2" class="text-center">PPH YG SDH DIBAYAR</th>
+            <th rowspan="2" class="text-center">KEKURANGAN PPH 21</th>
+        </tr>
+        <tr>
+            <th class="text-center">TERATUR</th>
+            <th class="text-center">TIDAK TERATUR</th>
+            <th class="text-center">TOTAL</th>
+            <th class="text-center">GAJI</th>
+            <th class="text-center">IURAN PENSIUN</th>
+        </tr>
+    </thead>
+    <tbody>
+        @php
+            $totalTeratur = 0;
+            $totalTidakTeratur = 0;
+            $totalPenghasilanBruto = 0;
+            $totalBiayaJabatan = 0;
+            $totalIuranPensiun = 0;
+            $totalPenghasilanNeto = 0;
+            $totalPenghasilanNetoSetahun = 0;
+            $totalPtkp = 0;
+            $totalPkp = 0;
+            $totalPph21Terutang = 0;
+            $totalPph21Dibayar = 0;
+            $totalKekuranganPph21 = 0;
+        @endphp
+        @forelse ($data as $key => $item)
+            @php
+                $totalTeratur += $item->penghasilan_bruto->penghasilan_rutin;
+                $totalTidakTeratur += $item->penghasilan_tidak_rutin;
+                $totalPenghasilanBruto += $item->penghasilan_bruto->total_penghasilan;
+                $totalBiayaJabatan += $item->pengurangan_penghasilan->biaya_jabatan;
+                $totalIuranPensiun += $item->pengurangan_penghasilan->total_pengurangan_bruto;
+                $totalPenghasilanNeto += $item->perhitungan_pph21->total_penghasilan_neto;
+                $totalPenghasilanNetoSetahun += $item->perhitungan_pph21->jumlah_penghasilan_neto_pph21;
+                $totalPtkp += $item->ptkp->ptkp_tahun ?? 0;
+                $totalPkp += $item->perhitungan_pph21->pph_pasal_21->penghasilan_kena_pajak_setahun;
+                $totalPph21Terutang += $item->perhitungan_pph21->pph_pasal_21->pph_21_terutang;
+                $totalPph21Dibayar += $item->perhitungan_pph21->pph_pasal_21->pph_telah_dilunasi;
+                $totalKekuranganPph21 += $item->perhitungan_pph21->pph_pasal_21->pph_harus_dibayar;
+
+                $kurungDepan = '';
+                $kurungBelakang = '';
+                if ($item->perhitungan_pph21) {
+                    if ($item->perhitungan_pph21->pph_pasal_21->pph_harus_dibayar > 0) {
+                        $kurungDepan = '(';
+                        $kurungBelakang = ')';
+                    }
+                }
+            @endphp
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $item->nama_karyawan }}</td>
+                <td>{{ $item->alamat_ktp }}</td>
+                <td>{{ $item->jk == 'Laki-laki' ? 'L' : 'P' }}</td>
+                <td>{{ $item->display_jabatan }}</td>
+                <td>{{ $item->keluarga->status_kawin ?? '-' }}</td>
+                <td>{{ $item->total_masa_kerja ? $item->total_masa_kerja : 0 }}</td>
+                <td class="text-right">{{ $item->penghasilan_bruto->penghasilan_rutin }}</td>
+                <td class="text-right">{{ $item->penghasilan_bruto->penghasilan_tidak_rutin }}</td>
+                <td class="text-right">{{ $item->penghasilan_bruto->total_penghasilan }}</td>
+                <td class="text-right">{{ $item->pengurangan_penghasilan ? $item->pengurangan_penghasilan->biaya_jabatan : 0 }}</td>
+                <td class="text-right">{{ $item->pengurangan_penghasilan ? $item->pengurangan_penghasilan->total_pengurangan_bruto : 0 }}</td>
+                <td class="text-right">{{ $item->perhitungan_pph21 ? $item->perhitungan_pph21->total_penghasilan_neto : 0 }}</td>
+                <td class="text-right">{{ $item->perhitungan_pph21 ? $item->perhitungan_pph21->jumlah_penghasilan_neto_pph21 : 0 }}</td>
+                <td class="text-right">{{ $item->ptkp ? $item->ptkp->ptkp_tahun : 0 }}</td>
+                <td class="text-right">{{ $item->perhitungan_pph21 ? $item->perhitungan_pph21->pph_pasal_21->penghasilan_kena_pajak_setahun : 0 }}</td>
+                <td class="text-right">{{ $item->perhitungan_pph21 ? $item->perhitungan_pph21->pph_pasal_21->pph_21_terutang : 0 }}</td>
+                <td class="text-right">{{ $item->perhitungan_pph21 ? $item->perhitungan_pph21->pph_pasal_21->pph_telah_dilunasi : 0 }}</td>
+                <td class="text-right">{{ $item->perhitungan_pph21 ? $kurungDepan . $item->perhitungan_pph21->pph_pasal_21->pph_harus_dibayar . $kurungBelakang : 0 }}</td>
+            </tr>
+        @empty
+            <tr>
+                <td colspan="18" class="text-center">Maaf Data Tidak Tersedia</td>
+            </tr>
+        @endforelse
+    </tbody>
+    <tfoot>
+        <tr>
+            <th colspan="7" class="text-center">Grand Total</th>
+            <th class="text-right">{{ $totalTeratur }}</th>
+            <th class="text-right">{{ $totalTidakTeratur }}</th>
+            <th class="text-right">{{ $totalPenghasilanBruto }}</th>
+            <th class="text-right">{{ $totalBiayaJabatan }}</th>
+            <th class="text-right">{{ $totalIuranPensiun }}</th>
+            <th class="text-right">{{ $totalPenghasilanNeto }}</th>
+            <th class="text-right">{{ $totalPenghasilanNetoSetahun }}</th>
+            <th class="text-right">{{ $totalPtkp }}</th>
+            <th class="text-right">{{ $totalPkp }}</th>
+            <th class="text-right">{{ $totalPph21Terutang }}</th>
+            <th class="text-right">{{ $totalPph21Dibayar }}</th>
+            <th class="text-right">{{ $totalKekuranganPph21 }}</th>
+        </tr>
+    </tfoot>
+</table>
---
+++ resources/views/rekap-tetap/index.blade.php
@@ -0,0 +1,264 @@
+@extends('layouts.template')
+@push('style')
+    <style>
+        table th {
+            border: 1px solid #e3e3e3 !important;
+        }
+    </style>
+@endpush
+
+@section('content')
+    <div class="d-lg-flex justify-content-between w-100 p-3">
+        <div class="card-header">
+            <h5 class="card-title">Rekap Tetap</h5>
+            <p class="card-title"><a href="">Laporan </a> > Rekap Tetap</p>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="col">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="table-responsive overflow-hidden content-center">
+                        <form id="form" method="get">
+                            <div class="row">
+                                @if (auth()->user()->hasRole('cabang'))
+                                    <input type="hidden" name="kantor" value="cabang">
+                                @else
+                                    <div class="col">
+                                        <div class="form-group">
+                                            <label for="">Kantor<span class="text-danger">*</span></label>
+                                            <select name="kantor" id="kantor"
+                                                class="form-control">
+                                                <option value="0">-- Pilih kantor --</option>
+                                                <option value="keseluruhan" @if(\Request::get('kantor') == 'keseluruhan') selected @endif
+                                                    {{old('kantor') == 'keseluruhan' ? 'selected' : ''}}>Keseluruhan</option>
+                                                <option value="pusat" @if(\Request::get('kantor') == 'pusat') selected @endif
+                                                    {{old('kantor') == 'pusat' ? 'selected' : ''}}>Pusat</option>
+                                                <option value="cabang" @if(\Request::get('kantor') != '' && \Request::get('kantor') != 'pusat') selected @endif
+                                                    {{old('kantor') == 'cabang' ? 'selected' : ''}}>Cabang</option>
+                                            </select>
+                                            @error('kantor')
+                                                <small class="text-danger">{{ucfirst($message)}}</small>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                @endif
+                                @if (auth()->user()->hasRole('cabang'))
+                                    <input type="hidden" name="cabang" value="{{auth()->user()->kd_cabang}}">
+                                @else
+                                    <div class="col cabang-input @if(\Request::get('kantor') == 'pusat' || \Request::get('kantor') == '0')d-none @endif">
+                                        <div class="form-group">
+                                            <label for="">Cabang<span class="text-danger">*</span></label>
+                                            <select name="cabang" id="cabang"
+                                                class="form-control select2">
+                                                <option value="0">-- Pilih cabang --</option>
+                                                @foreach ($cabang as $item)
+                                                    <option value="{{$item->kd_cabang}}" @if(\Request::get('cabang') == $item->kd_cabang) selected @endif>{{$item->nama_cabang}}</option>
+                                                @endforeach
+                                            </select>
+                                            @error('cabang')
+                                                <small class="text-danger">{{ucfirst($message)}}</small>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                @endif
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="">Tahun<span class="text-danger">*</span></label>
+                                        <select name="tahun" id="tahun"
+                                            class="form-control">
+                                            <option value="0">-- Pilih tahun --</option>
+                                            @php
+                                                $sekarang = date('Y');
+                                                $awal = $sekarang - 5;
+                                                $akhir = $sekarang + 5;
+                                            @endphp
+                                            @for($i=$awal;$i<=$akhir;$i++)
+                                                <option value="{{$i}}" @if(\Request::get('tahun') == $i) selected @endif>{{$i}}</option>
+                                            @endfor
+                                        </select>
+                                        @error('tahun')
+                                            <small class="text-danger">{{ucfirst($message)}}</small>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="d-flex justify-content-end">
+                                @if (\Request::has('kantor') && count($data) > 0)
+                                    <div class="mr-2" id="btn-download">
+                                        <a href="{{ route('download-rekap-tetap') }}" download
+                                            class="m-0 btn btn-lg is-btn btn-warning">
+                                            <span style="font-size: 14px;">Excel</span>
+                                        </a>
+                                    </div>
+                                @endif
+                                <div>
+                                    <input type="submit" value="Tampilkan" class="is-btn is-primary">
+                                </div>
+                            </div>
+                            @if (\Request::has('kantor'))
+                                <div class="d-flex justify-content-between mb-4">
+                                    <div class="p-2 mt-4">
+                                    <label for="page_length" class="mr-3 text-sm text-neutral-400">show</label>
+                                    <select name="page_length" id="page_length"
+                                        class="border px-4 py-2 cursor-pointer rounded appearance-none text-center">
+                                        <option value="10"
+                                            @isset($_GET['page_length']) {{ $_GET['page_length'] == 10 ? 'selected' : '' }} @endisset>
+                                            10</option>
+                                        <option value="20"
+                                            @isset($_GET['page_length']) {{ $_GET['page_length'] == 20 ? 'selected' : '' }} @endisset>
+                                            20</option>
+                                        <option value="50"
+                                            @isset($_GET['page_length']) {{ $_GET['page_length'] == 50 ? 'selected' : '' }} @endisset>
+                                            50</option>
+                                        <option value="100"
+                                            @isset($_GET['page_length']) {{ $_GET['page_length'] == 100 ? 'selected' : '' }} @endisset>
+                                            100</option>
+                                    </select>
+                                    <label for="" class="ml-3 text-sm text-neutral-400">entries</label>
+                                    </div>
+                                    <div class="p-2">
+                                    <label for="q">Cari</label>
+                                    <input type="search" name="q" id="q" placeholder="Cari nama karyawan disini..."
+                                        class="form-control p-2" value="{{isset($_GET['q']) ? $_GET['q'] : ''}}"
+                                        style="width: 300px;">
+                                    </div>
+                                </div>
+                                @php
+                                    $page = isset($_GET['page']) ? $_GET['page'] : 1;
+                                    $page_length = isset($_GET['page_length']) ? $_GET['page_length'] : 10;
+                                    $start = $page == 1 ? 1 : ($page * $page_length - $page_length) + 1;
+                                    $end = $page == 1 ? $page_length : ($start + $page_length) - 1;
+                                @endphp
+                                @if (\Request::has('tahun'))
+                                    <div class="table-responsive">
+                                        @include('rekap-tetap.table.table')
+
+                                    </div>
+                                @endif
+                                <div class="d-flex justify-content-between">
+                                    <div>
+                                        Showing {{$start}} to {{$end}} of {{$data->total()}} entries
+                                    </div>
+                                    <div>
+                                        @if ($data instanceof \Illuminate\Pagination\LengthAwarePaginator)
+                                        {{ $data->appends(\Request::except('page'))->links('pagination::bootstrap-4') }}
+                                        @endif
+                                    </div>
+                                </div>
+
+                            @endif
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('script')
+    <script>
+        var selected_kantor = $('#kantor').val()
+        if (selected_kantor == '0' || selected_kantor == 'pusat') {
+            // Hide cabang
+            $('.cabang-input').addClass('d-none')
+        }
+        else {
+            // Show cabang
+            $('.cabang-input').removeClass('d-none')
+        }
+
+        $('#kantor').on('change', function() {
+            $('#btn-download').addClass('d-none')
+        })
+
+        $('#cabang').on('change', function() {
+            $('#btn-download').addClass('d-none')
+        })
+
+        $('#bulan').on('change', function() {
+            $('#btn-download').addClass('d-none')
+        })
+
+        $('#tahun').on('change', function() {
+            $('#btn-download').addClass('d-none')
+        })
+
+        $('#kategori').on('change', function() {
+            $('#btn-download').addClass('d-none')
+        })
+
+        const formatRupiahPayroll = (angka) => {
+            let reverse = angka.toString().split('').reverse().join('');
+            let ribuan = reverse.match(/\d{1,3}/g);
+            ribuan = ribuan.join('.').split('').reverse().join('');
+            return `${ribuan}`;
+        }
+
+        $('.show-data').on('click',function(e) {
+            const targetId = $(this).data("target-id");
+            const data = $(this).data('json');
+            $('#table-tunjangan-tidak > tbody').empty();
+            $('#table-tunjangan-total-tidak thead').empty();
+
+            $('#table-tunjangan > tbody').empty();
+            $('#table-tunjangan-total thead ').empty();
+
+            $('#nip').html(`${data.nip}`)
+            $('#nama').html(`${data.nama_karyawan}`)
+            $('#no_rekening').html(`${data.no_rekening != null ? data.no_rekening : '-'}`)
+
+            var nominal = 0;
+            var tableTunjangan = `
+                    <tr style="border:1px solid #e3e3e3">
+                        <td>Gaji Pokok</td>
+                        <td id="gaji_pokok" class="text-right">${formatRupiahPayroll(data.gaji['total_gaji'])}</td>
+                    </tr>
+            `;
+            // START TUNJANGAN TERATUR
+            $.each(data.tunjangan, function( key, value ) {
+                nominal += value.pivot.nominal ;
+                tableTunjangan += `
+                    <tr style="border:1px solid #e3e3e3">
+                        <td class="text-left fw-bold">${value.nama_tunjangan}</td>
+                        <td class="text-right">${formatRupiahPayroll(value.pivot.nominal)}</td>
+                    </tr>
+                `
+            });
+            $("#table-tunjangan tbody").append(tableTunjangan);
+
+            var tableTotalTunjanganTeratur = `
+                <tr>
+                    <th width="60%">GAJI POKOK + PENGHASILAN TERATUR</th>
+                    <th class="text-right ">${formatRupiahPayroll(nominal + data.gaji['total_gaji'])}</th>
+                </tr>
+            `
+            $("#table-tunjangan-total thead").append(tableTotalTunjanganTeratur);
+            // END TUNJANGAN TERATUR
+        })
+        function showModal(identifier) {
+        }
+
+        $('#kantor').on('change', function() {
+            const selected = $(this).val()
+
+            if (selected == 'cabang') {
+                $('.cabang-input').removeClass('d-none')
+            }
+            else {
+                $('#cabang option[value="0"]').attr("selected", "selected");
+                $('.cabang-input').addClass('d-none')
+            }
+        })
+
+        $('#page_length').on('change', function() {
+            $('#form').submit()
+        })
+
+        $('#form').on('submit', function() {
+            $('.loader-wrapper').css('display: none;')
+            $('.loader-wrapper').addClass('d-block')
+            $(".loader-wrapper").fadeOut("slow");
+        })
+    </script>
+@endpush
