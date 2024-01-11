e88273f2e663566be5c68bdf38c1da8d2c8e8c0f
oong <37205723+oong26@users.noreply.github.com>
2024-01-11 13:11:27 +0700

    develop-payroll : change loading color

--- resources/views/components/old/loader.blade.php
+++ resources/views/components/old/loader.blade.php
@@ -1,7 +1,7 @@
 <div class="loader-wrapper">
     <div class="wrapper-loading">
         <center>
-            <div class="la-line-spin-clockwise-fade la-dark la-2x">
+            <div class="la-line-spin-clockwise-fade la-light la-2x">
                 <div></div>
                 <div></div>
                 <div></div>
@@ -13,7 +13,7 @@
             </div>
         </center>
         <div>
-            <h4 class="text-center">Harap Tunggu, Data sedang diproses...</h4>
+            <h4 class="text-center text-light">Harap Tunggu, Data sedang diproses...</h4>
         </div>
     </div>
 </div>
