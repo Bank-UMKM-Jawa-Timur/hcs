294a303e2cc958b2f250d44280129105c55fa7af
oong <37205723+oong26@users.noreply.github.com>
2024-02-13 23:44:13 +0700

    feat-penonaktifan-ikut-proses : adding nip foreign key to some tables
    Adding foreign key cascade on update and delete to needed tables

---
+++ database/migrations/2024_02_13_233547_add_nip_foreign_on_pejabat_sementara_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Disable foreign key checks
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        // Add the foreign key constraint
+        Schema::table('pejabat_sementara', function (Blueprint $table) {
+            $table->foreign('nip')
+                    ->references('nip')
+                    ->on('mst_karyawan')
+                    ->cascadeOnUpdate()
+                    ->cascadeOnDelete();
+        });
+
+        // Enable foreign key checks
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pejabat_sementara', function(Blueprint $table) {
+            $table->dropForeign('pejabat_sementara_nip_foreign');
+        });
+    }
+};
