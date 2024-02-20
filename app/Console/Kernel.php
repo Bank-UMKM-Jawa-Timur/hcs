5b23f07787c98cd702c2fee319647cd4de188dad
oong <37205723+oong26@users.noreply.github.com>
2024-02-20 21:13:28 +0700

    develop-master : create log for cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -16,11 +17,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        Log::info('Cron Job Started');
         $endDate = Carbon::now()->daysInMonth;
 
         // Backup the database on every 26 or end of day in month
         $schedule->command('db:backup')->monthlyOn(26, '00:01')->appendOutputTo(storage_path('logs/laravel.log'));
         $schedule->command('db:backup')->monthlyOn($endDate, '00:01')->appendOutputTo(storage_path('logs/laravel.log'));
+        Log::info('Cron Job Ended');
     }
 
     /**
