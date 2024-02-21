1005533c79cf4337b3ffe383b9ec5db32c94e46b
oong <37205723+oong26@users.noreply.github.com>
2024-02-21 15:36:58 +0700

    fix-penambah-bruto : fix variable declaration
    - store function on gajiperbulancontroller
    - getJamsostek function on hitungpph class

--- app/Http/Controllers/GajiPerBulanController.php
+++ app/Http/Controllers/GajiPerBulanController.php
@@ -1460,7 +1460,9 @@ class GajiPerBulanController extends Controller
                     $jht = 0;
                     $jkm = 0;
                     $jp_penambah = 0;
-                    if(!$item->tanggal_penonaktifan && $item->kpj){
+                    $bpjs_kesehatan = 0;
+                    $jamsostek = 0;
+                    if($item->kpj){
                         $jkk = floor(($persen_jkk / 100) * $total_gaji);
                         $jht = floor(($persen_jht / 100) * $total_gaji);
                         $jkm = floor(($persen_jkm / 100) * $total_gaji);
