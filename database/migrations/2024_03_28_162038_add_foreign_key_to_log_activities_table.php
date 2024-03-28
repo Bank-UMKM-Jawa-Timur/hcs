b939bc9f372b8b996100f5aacc1db2272ab5be85
oong26 <37205723+oong26@users.noreply.github.com>
2024-03-28 16:22:05 +0700

    feat/log-activity : create log activity migration

---
+++ database/migrations/2024_03_28_162038_add_foreign_key_to_log_activities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('log_activities', function (Blueprint $table) {
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('log_activities', function (Blueprint $table) {
+            $table->dropForeign('log_activities_user_id_foreign');
+        });
+    }
+};
