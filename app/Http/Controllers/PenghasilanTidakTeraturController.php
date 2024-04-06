3db040467cdd39798356b74a39e2be936a9c9ed5
LuciferLovesMe <abhiemsmile@gmail.com>
2024-04-06 14:08:28 +0700

    feat/log-activity: add log to lock penghasilan tidak teratur

--- app/Http/Controllers/PenghasilanTidakTeraturController.php
+++ app/Http/Controllers/PenghasilanTidakTeraturController.php
@@ -946,6 +946,19 @@ class PenghasilanTidakTeraturController extends Controller
         if (!auth()->user()->can('penghasilan - lock - penghasilan tidak teratur')) {
             return view('roles.forbidden');
         }
+
+        // Record to log activity
+        $name = Auth::guard('karyawan')->check() ? auth()->guard('karyawan')->user()->nama_karyawan : auth()->user()->name;
+        $tunjanganShow = DB::table('mst_tunjangan')->where('id', $request->id_tunjangan)->first()?->nama_tunjangan;
+        $kantorShow = DB::table('penghasilan_tidak_teratur')->where('id_tunjangan', $request->id_tunjangan)
+            ->where('bulan', (int) Carbon::parse($request->get('tanggal'))->format('m'))
+            ->where('tahun', (int) Carbon::parse($request->get('tanggal'))->format('Y'))
+            ->where('penghasilan_tidak_teratur.created_at', $request->tanggal)
+            ->join('mst_cabang', 'mst_cabang.kd_cabang', 'penghasilan_tidak_teratur.kd_entitas')
+            ->first()?->nama_cabang;
+        $activity = "Pengguna <b>$name</b> melakukan lock penghasilan tidak teratur untuk kantor <b>$kantorShow</b> tunjangan <b>$tunjanganShow</b> tanggal <b>$request->tanggal</b>";
+        LogActivity::create($activity);
+
         $repo = new PenghasilanTidakTeraturRepository;
         $body = [
             'id_tunjangan' => $request->get('id_tunjangan'),
